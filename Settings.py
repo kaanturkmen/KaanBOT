ca98b2e4cf11c48e4e665b35ae2b5ac6472ee6da
katurkmen <kaanturkmen@protonmail.com>
2020-09-15 12:22:55 +0300

    add settings

---
+++ Settings.py
@@ -0,0 +1,32 @@
+
+BOT_TOKEN = ''
+COMMAND_PREFIX = ''
+SERVER_ID = 0
+SERVER_LOG_CHANNEL_ID = 741219189049655366
+ADMINS_USER_ID = 0
+CHANNEL_TO_UPDATE_ID = 0
+ADMIN_ROLE = ''
+MODERATOR_ROLE = ''
+
+"""
+This file is created for inputting constants.
+
+:param BOT_TOKEN: Bot token is a random key for your bot. It will be given in discord's developer menu.
+:param COMMAND_PREFIX: Command prefix is how you call your bot while you are using commands. For instance, .y or !y
+:param ADMIN_ROLE: Role name of admin. Keep in mind that it is case sensitive.
+:param MODERATOR_ROLE: Role name of moderator. Keep in mind that it is case sensitive.
+:param SERVER_ID: ID of your channel. (AKA. Guild)
+:param SERVER_LOG_CHANNEL_ID: ID of your logs channel. It should be a text channel.
+:param ADMINS_USER_ID: Admin's ID for mentioning you when someone joins or leaves.
+:param CHANNEL_TO_UPDATE_ID: Voice channel for updating how many users are in the server.
+
+
+:type BOT_TOKEN: String
+:type COMMAND_PREFIX: Char or String
+:type ADMIN_ROLE: Char or String
+:type MODERATOR_ROLE: Char or String
+:type SERVER_ID: Int
+:type SERVER_LOG_CHANNEL_ID: Int
+:type ADMINS_USER_ID: Int
+:type CHANNEL_TO_UPDATE_ID: Int
+"""
